e5add47c60a28f0607ce7d3e3352f865c6da9a73
ezah02021996@gmail.com <ezah02021996@gmail.com>
2020-03-23 21:12:51 +0200

    adds logout link

--- udemy_app/src/containers/Orders/Orders.js
+++ udemy_app/src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrders();
+    this.props.onFetchOrders(this.props.token);
   }
   render() {
     let orders = <Spinner />;
@@ -31,13 +31,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
   return {
     orders: state.order.orders,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
   };
 };
 
 const mapDispatchToprops = dispatch => {
   return {
-    onFetchOrders: () => dispatch(actions.fetcOrders())
+    onFetchOrders: token => dispatch(actions.fetcOrders(token))
   };
 };
 
--- udemy_app/src/store/actions/index.js
+++ udemy_app/src/store/actions/index.js
@@ -4,4 +4,4 @@ export {
   initIngredients
 } from "./burgerBuilder";
 export { purchaseBurger, purchaseInit, fetcOrders } from "./order";
-export { auth } from "./auth";
+export { auth, logout } from "./auth";
