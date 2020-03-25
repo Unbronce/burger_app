4846466fc01106d02a83488da52bc9351d7fa84c
ezah02021996@gmail.com <ezah02021996@gmail.com>
2020-03-25 20:01:54 +0200

    adds auth to the project

--- udemy_app/src/store/actions/index.js
+++ udemy_app/src/store/actions/index.js
@@ -3,5 +3,5 @@ export {
   removeIngredient,
   initIngredients
 } from "./burgerBuilder";
-export { purchaseBurger, purchaseInit, fetcOrders } from "./order";
-export { auth, logout } from "./auth";
+export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
