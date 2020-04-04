2a4e06b3881df6f6b441424f29d15e233b2fd6c7
ezah02021996@gmail.com <ezah02021996@gmail.com>
2020-04-04 19:00:06 +0300

    turns all project into functional components

--- udemy_app/src/components/Burger/OrderSummary/OrderSummary.js
+++ udemy_app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import Aux from "../../../hoc/Auxiliary/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-class OrderSummary extends Component {
-  render() {
-    const ingridientSummary = Object.keys(this.props.ingridients).map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-          {this.props.ingridients[igKey]}
-        </li>
-      );
-    });
+const OrderSummary = (props) => {
+  const ingridientSummary = Object.keys(props.ingridients).map((igKey) => {
     return (
-      <Aux>
-        <h3>A delicious burger with the following ingridients:</h3>
-        <ul>{ingridientSummary}</ul>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
-          CANCEL
-        </Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>
-          CONTINUE
-        </Button>
-      </Aux>
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+        {props.ingridients[igKey]}
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <Aux>
+      <h3>A delicious burger with the following ingridients:</h3>
+      <ul>{ingridientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
 
 export default OrderSummary;
